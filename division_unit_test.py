41acd9052f96ca90ff66a9b1a214c33e17d4dba9
Pranjal Srivastava <143828394+Pranjal-Srivastava-2023@users.noreply.github.com>
2023-09-15 12:22:49 -0400

    Added comments to division_unit_test.py

--- division_unit_test.py
+++ division_unit_test.py
@@ -1,10 +1,18 @@
-def division(x,y):
-  if y == 0:
-    return "ERROR"
-  else:
-    return x/y
+# Define a function called 'division' that takes two arguments, 'x' and 'y'
+def division(x, y):
+    # Check if 'y' is equal to 0
+    if y == 0:
+        # If 'y' is 0, return the string "ERROR" (division by zero is undefined)
+        return "ERROR"
+    else:
+        # If 'y' is not 0, return the result of 'x' divided by 'y'
+        return x / y
 
-assert (division(2,1)) == 2
-assert (division(0,5)) == 0
-assert (division(1,2)) == 0.5
-assert (division(1,0)) == "ERROR"
+# Assert statements are used to test the 'division' function
+# They check if the function behaves as expected for specific inputs
+
+# Assert that division(2, 1) should return 2 and so on...
+assert division(2, 1) == 2
+assert division(0, 5) == 0
+assert division(1, 2) == 0.5
+assert division(1, 0) == "ERROR"
